38e6bd96736270c15f3ebe5078d0a7172332f188
Rishika Idnani <idnani.ri@husky.neu.edu>
2016-02-19 05:29:38 -0500

    libraries

--- NationalVaccineDistributionSystem/src/UserInterface/Distributor/DistributionCentreWorkArea.java
+++ NationalVaccineDistributionSystem/src/UserInterface/Distributor/DistributionCentreWorkArea.java
@@ -19,12 +19,12 @@ import Business.WorkRequest.ShipmentToDistributionCentre;
 import Business.WorkRequest.ShipmentToSite;
 import Business.WorkRequest.WorkRequest;
 import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+//import javax.mail.Message;
+//import javax.mail.MessagingException;
+//import javax.mail.Session;
+//import javax.mail.Transport;
+//import javax.mail.internet.InternetAddress;
+//import javax.mail.internet.MimeMessage;
 //import java.util.Properties;
 //import javax.mail.Message;
 //import javax.mail.MessagingException;
@@ -63,24 +63,24 @@ public class DistributionCentreWorkArea extends javax.swing.JPanel {
         populateSiteList();
     }
 
-    public void sendEmail() {
-        Properties properties = System.getProperties();
-        properties.setProperty("mail.smtp.host", host);
-        Session session = Session.getDefaultInstance(properties);
-
-        try {
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
-            message.addRecipient(Message.RecipientType.TO,
-                    new InternetAddress(to));
-            message.setSubject("This is the Subject Line!");
-            message.setText("This is actual message");
-            Transport.send(message);
-            System.out.println("Sent message successfully....");
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
-        }
-    }
+//    public void sendEmail() {
+//        Properties properties = System.getProperties();
+//        properties.setProperty("mail.smtp.host", host);
+//        Session session = Session.getDefaultInstance(properties);
+//
+//        try {
+//            MimeMessage message = new MimeMessage(session);
+//            message.setFrom(new InternetAddress(from));
+//            message.addRecipient(Message.RecipientType.TO,
+//                    new InternetAddress(to));
+//            message.setSubject("This is the Subject Line!");
+//            message.setText("This is actual message");
+//            Transport.send(message);
+//            System.out.println("Sent message successfully....");
+//        } catch (MessagingException mex) {
+//            mex.printStackTrace();
+//        }
+//    }
 
     public void populateSiteList() {
         DefaultListModel model = new DefaultListModel();
